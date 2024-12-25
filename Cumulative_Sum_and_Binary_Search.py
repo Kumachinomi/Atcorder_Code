1b67b6155fdac249fe45a9b044fb756a5689c5d4
Kumachinomi <nomizu7412@gmail.com>
2024-12-25 09:38:58 +0900

    二分探索・累積和2

--- Cumulative_Sum_and_Binary_Search.py
+++ Cumulative_Sum_and_Binary_Search.py
@@ -20,3 +20,27 @@ for i in range(2 * n):
     result = True
 
 print("Yes" if result else "No")
+
+
+
+
+
+
+#二分探索・累積和 2
+import bisect
+
+n,q = map(int,input().split())
+r = list(map(int,input().split()))
+r.sort()
+
+s = [0] * (n + 1)
+s[0] = 0
+for i in range(n):
+  s[i + 1] = s[i] + r[i]
+
+
+  
+for i in range(q):
+  x = int(input())
+  it = bisect.bisect_right(s,x)-1
+  print(it)
